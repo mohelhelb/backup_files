0d33133c7a20afefb3fcf6c525ca5c09704f3736
Mohamed El Hammouti El Berkani <mohelhelb@gmail.com>
2022-02-09 17:23:37 +0100

    discard long options

--- backup_files.sh
+++ backup_files.sh
@@ -12,7 +12,7 @@ function Help {
 	printf "\nNAME\n\n${script_filename} - make a backup of files\n\n"
 	printf "SYNOPSIS\n\n${script_filename} [-d dir|-h]\n\n"
 	printf "DESCRIPTION\n\nThis Bash shell script is aimed at backing up the files in the config_file.txt file. By default, the parent directory that serves the purpose of containing the directories that, in turn, contain the tarfiles, backup_files directory, is created in the user's home directory (/home/'user'/backup_files). However, it can also be specified by invoking the script along with the appropriate option (d). Since the config_file.txt file plays a major role in that it contains the user-chosen files to be backed up, the user is asked to create it, if it does not exist, and/or populate it, if it is empty, at runtime. The non-existent files and duplicate ones in the config_file.txt file are discarded; only the valid files are archived and compressed into a tarfile in a directory within the backup_files directory. It's also worth mentioning that running this script generates separate tarfiles provided that it is executed on different dates. Otherwise, the existing tarfile is overwritten by the new created one.\n\n"
-	printf "OPTIONS\n\n-d, --directory dir\n\tSet user-defined archive directory.\n\n-h, --help\n\tDisplay this help and exit.\n\n"
+	printf "OPTIONS\n\n-d dir\n\tSet user-defined archive directory.\n\n-h \n\tDisplay this help and exit.\n\n"
 }
 # Retrieve input from user and write it to ACF
 function input_fls {
@@ -78,30 +78,30 @@ day=$(date +%d)
 # Create array of valid/invalid files 
 valid_files=()
 invalid_files=()
+# Script filename
+script_filename=$(basename $0)
 # Set default Archive Directory
 arch_dir="/home/${user}/backup_files"
 # Set user-defined Archive Directory
 while getopts :d:h opt
 do
 	case ${opt} in
-		d|directory)
+		d)
 			arch_dir="${OPTARG}"
 			if [[ -d ${arch_dir} ]] && [[ -w ${arch_dir} ]]; then
 				continue
 			else
-				printf "Bad Usage: $0 [-d dir]\nTry: Create dir and/or grant write permission to dir\n"
+				printf "Bad Usage: ${script_filename} [-d dir|-h]\nTry: Create dir and/or grant write permission to dir\n"
 				exit
 			fi
 			;;
-		h|help)
-			# Script filename
-			script_filename=$(basename "$0")
+		h)
 			# Display help and exit
 			Help "${script_filename}"
 			exit
 			;;
 		*)
-			printf "Bad Usage: $0 [-d dir]\nTry: Invoke valid option along with dir\n"
+			printf "Bad Usage: ${script_filename} [-d dir|-h]\nTry: Invoke valid option along with dir\n"
 			exit
 			;;
 	esac
